9d18e36f6d9dd2a3a7b7602e7f3efb3316c95ad2
ahmadkheder <ahmadkhederabdulatef@gmail.com>
2020-09-09 01:00:26 +0300

    class-02

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ const Notes  = require('./lib/notes.js');
 const userInput= new Input();
 const note = new Notes();
 
-let UserNote = note.execute(userInput);
+let UserNote = note.execute({ action: process.argv.slice(2)[0].slice(1), payload: process.argv.slice(2)[1] });
+
